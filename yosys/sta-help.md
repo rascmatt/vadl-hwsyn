76ae8d16e828de52d467579cb1891baa6c5c8620
Matthias Raschhofer <matthias@matthiass-air.home>
2025-10-26 18:25:36 +0100

    Make OpenSTA timings work

---
+++ yosys/sta-help.md
@@ -0,0 +1,11 @@
+Running OpenSTA:
+
+```
+docker run -i -v .:/data opensta
+```
+
+Run with input script
+
+```
+docker run -i -v .:/data opensta < sta.tcl
+```
---
+++ yosys/sta.tcl
@@ -0,0 +1,10 @@
+read_liberty /data/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+read_verilog /data/build/decode_vdt_manual.v
+link_design InstructionDecoder
+
+# Timings
+create_clock -name core_clk -period 5.0 [get_ports clock]
+set_input_delay  1.0 -clock [get_clocks core_clk] [all_inputs]
+set_output_delay 1.0 -clock [get_clocks core_clk] [all_outputs]
+
+report_checks
